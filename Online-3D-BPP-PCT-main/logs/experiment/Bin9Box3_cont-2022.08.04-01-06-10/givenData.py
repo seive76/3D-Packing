45ef9def634b15da55fbee6d0dbe5a4d895ee801
cimoody <cristina.isadora.moody@gmail.com>
2022-08-05 15:52:42 -0400

    Add repo from alexfrom0815/Online-3D-BPP-PCT

--- Online-3D-BPP-PCT-main/logs/experiment/Bin9Box3_cont-2022.08.04-01-06-10/givenData.py
+++ Online-3D-BPP-PCT-main/logs/experiment/Bin9Box3_cont-2022.08.04-01-06-10/givenData.py
@@ -8,8 +8,8 @@ higher = 3 ## Change to have uniform boxes was 1-5, now 2-2
 resolution = 1
 item_size_set = []
 for i in range(lower, higher + 1):
-    for j in range(lower, higher + 1): ## Changing from + 1 to + 4 for large flat boxes.
-        for k in range(lower, higher + 1): ## Changing from + 1 to + 4 for large flat boxes.
+    for j in range(lower, higher + 1):
+        for k in range(lower, higher + 1):
                 item_size_set.append((i * resolution, j * resolution , k *  resolution))
 
 # If you want to sample item sizes from a uniform distribution in continuous domain,
--- Online-3D-BPP-PCT-main/logs/experiment/test-2022.08.03-17-53-29/givenData.py
+++ Online-3D-BPP-PCT-main/logs/experiment/test-2022.08.03-17-53-29/givenData.py
@@ -1,15 +1,15 @@
 # container_size: A vector of length 3 describing the size of the container in the x, y, z dimension.
 # item_size_set:  A list records the size of each item. The size of each item is also described by a vector of length 3.
 
-container_size = [9,9,9]
+container_size = [10,10,10]
 
-lower = 3
-higher = 3 ## Change to have uniform boxes was 1-5, now 2-2
+lower = 1
+higher = 5
 resolution = 1
 item_size_set = []
 for i in range(lower, higher + 1):
-    for j in range(lower, higher + 1): ## Changing from + 1 to + 4 for large flat boxes.
-        for k in range(lower, higher + 1): ## Changing from + 1 to + 4 for large flat boxes.
+    for j in range(lower, higher + 1):
+        for k in range(lower, higher + 1):
                 item_size_set.append((i * resolution, j * resolution , k *  resolution))
 
 # If you want to sample item sizes from a uniform distribution in continuous domain,
